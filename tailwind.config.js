7a3884a7d5f77138e0722310662c700bfa42ff0e
MicKilldev <Mickaelgarde143@gmail.com>
2026-02-24 15:03:30 +0800

    feat: Implement dark mode functionality with theme toggling, Tailwind CSS configuration, and new UI assets.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ tailwind.config = {
             fontFamily: {
                 Outfit: ["Outfit", "sans-serif"],
                 Ovo: ["Ovo", "serif"]
+            },
+            colors: {
+                lightHover: '#fcf4ff',
+                darkHover: '#2a004a',
+                darkTheme: '#11001F'
+            },
+            boxShadow: {
+                'black': '4px 4px 0 #000',
+                'white': '4px 4px 0 #fff',
             }
         }
-    }
+    },
+    darkMode: 'class'
 }
