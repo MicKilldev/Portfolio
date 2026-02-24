d824fd833b0261d005f56c48d8e2463f8bb482a3
MicKilldev <Mickaelgarde143@gmail.com>
2026-02-24 20:45:06 +0800

    Remove dark mode functionality by deleting `script.js` and all `dark:` classes from `index.html`, and fix minor HTML typos.

--- script.js
+++ script.js
@@ -5,15 +5,17 @@ const navLinks = document.querySelector('nav ul');
 function openMenu() {
     sideMenu.style.transform = 'translateX(-16rem)';
 }
-
 function closeMenu() {
     sideMenu.style.transform = 'translateX(16rem)';
 }
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
+    if(scrollY > 50) {
         navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50');
     } else {
         navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50');
+
     }
-});
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 tailwind.config = {
     theme: {
-        gridTemplateColumns: {
-            'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+        extend: {
+            gridTemplateColumns: {
+                'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+            },
+            fontFamily: {
+                Outfit: ["Outfit", "sans-serif"],
+                Ovo: ["Ovo", "serif"]
+            },
+            animation: {
+                spin_slow: 'spin 6s linear infinite'
+            },
+            colors: {
+                lightHover: '#fcf4ff',
+                darkHover: '#2a004a',
+            },
+            boxShadow: {
+                'black': '4px_4px_0_#000',
+                'white': '4px_4px_0_#fff'
+            }
         }
     }
 }
+
